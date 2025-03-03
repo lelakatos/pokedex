d89013ca6aae1dcaf292c08b208fff54c7125055
lelakatos <lakatos.levi@gmail.com>
2025-03-03 19:58:28 +0100

    refactor to repl.go

--- repl.go
+++ repl.go
@@ -1,9 +1,36 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func startRepl() {
+	for {
+		fmt.Print("Pokedex >")
+		scanner := bufio.NewScanner(os.Stdin)
+		ok := scanner.Scan()
+		if !ok {
+			fmt.Println("Error reading string")
+		}
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+
+	}
+
 }
