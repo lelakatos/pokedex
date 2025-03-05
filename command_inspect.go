646e28ed86aff2c27ebc6bd4b9be852df826e28d
lelakatos <lakatos.levi@gmail.com>
2025-03-05 20:08:08 +0100

    add inspect function

--- command_inspect.go
+++ command_inspect.go
@@ -3,6 +3,24 @@ package main
 import "fmt"
 
 func commandInspect(cfg *config, pokename string) error {
-	fmt.Println("inspect command called")
+	pokemon, exists := cfg.pokedex[pokename]
+	if !exists {
+		fmt.Printf("You haven't caught %s yet\n", pokename)
+		return nil
+	}
+
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %v\n", pokemon.Height)
+	fmt.Printf("Weight: %v\n", pokemon.Weight)
+	fmt.Println("Stats:")
+	for _, s := range pokemon.Stats {
+		fmt.Printf("  - %s: %v\n", s.Stat.Name, s.BaseStat)
+	}
+
+	fmt.Println("Types:")
+	for _, t := range pokemon.Types {
+		fmt.Printf("  - %s\n", t.Type.Name)
+	}
+
 	return nil
 }
